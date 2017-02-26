ea852ddd6fc6a51ca4c8f228b7a63123388a9b12
Hyce <jon.poullain@gmail.com>
2017-02-26 20:12:38 +0100

    full push bug  restart

---
+++ resources/views/iim.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.layout')
+
+@section('content')
+    <b>Institut de l'Internet et du Multimédia</b>
+    <br/>
+    @if(3>5)
+        chocolat
+    @else
+        mandarine
+    @endif
+@endsection
